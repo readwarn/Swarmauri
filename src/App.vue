a07579d8eccc5b22bddd0540b5fe684d2d53170b
Rilwan <akinyusuf5@gmail.com>
2024-12-10 00:45:13 +0100

    initial demo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    DEMO OF SWARMKIT-UI BY {{ name }}
+    <Button type="primary"> Submit </Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { Button } from "@swarmakit/vue";
+const name = ref("Rilwan");
+</script>
